f11056b0d3b34e4e970c48af70a7f615b4f4db7b
Luis Vicente Albarracin <luisalbarracin9727@gmail.com>
2022-01-02 19:07:10 -0500

    añadiendo primeras plantillas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { ErrorNoEncontradoComponent } from './publico/error-no-encontrado/error-no-encontrado.component';
+import { InicioComponent } from './publico/inicio/inicio.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: InicioComponent
+  },
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: '**', component: ErrorNoEncontradoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
